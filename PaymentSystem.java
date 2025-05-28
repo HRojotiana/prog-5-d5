6095b56dd10a463464cc799c1b5a3abde4b5aa89
HRojotiana <hei.rojotiana@gmail.com>
2025-05-28 09:13:51 +0300

    feat: add PaymentSystem methods

--- PaymentSystem.java
+++ PaymentSystem.java
@@ -1,3 +1,23 @@
 public class PaymentSystem {
+    protected float balance;
 
+    public PaymentSystem(){
+        this.balance = 0;
+    }
+
+    public float insert_money(float amount){
+        if(amount <= 0) {
+            throw new IllegalArgumentException("Please insert a valid amount");
+        } else {
+            balance += amount;
+            return balance;
+        }
+    }
+
+    public boolean validate_payment(float price) {
+        if(price > balance) {
+            throw new IllegalArgumentException("Insufficient balance");
+        }
+        return true;
+    }
 }
