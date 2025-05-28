0697b9164dfec03a87cb59b9436e097ac7a7cc22
HRojotiana <hei.rojotiana@gmail.com>
2025-05-28 09:29:08 +0300

    feat: update InventorySystem

--- InventorySystem.java
+++ InventorySystem.java
@@ -1,17 +1,35 @@
 public class InventorySystem {
     protected float waterQuantity;
-    protected float coffeQuantity;
+    protected float coffeeQuantity;
 
-    public InventorySystem(float waterQuantity, float coffeQuantity) {
+    public InventorySystem(float waterQuantity, float coffeeQuantity) {
         this.waterQuantity = waterQuantity;
-        this.coffeQuantity = coffeQuantity;
+        this.coffeeQuantity = coffeeQuantity;
+    }
+
+    public boolean check_resources(float requiredWater, float requiredCoffee) {
+        if((waterQuantity >= requiredWater) && (coffeeQuantity >= requiredCoffee)){
+            return true;
+        }
+        return false;
+    }
+
+    public void consume_resources(float requiredWater, float requiredCoffee) {
+        if (!check_resources(requiredWater, requiredCoffee)) {
+            throw new IllegalArgumentException("Out of resources");
+        } else {
+            waterQuantity -= requiredWater;
+            coffeeQuantity -= requiredCoffee;
+            System.out.println("Actual water quantity: "+ waterQuantity + " L" +
+                    "\n Actual coffee quantity: " + coffeeQuantity +  " kg");
+        }
     }
 
     @Override
     public String toString() {
         return "InventorySystem{" +
                 "waterQuantity=" + waterQuantity +
-                ", coffeQuantity=" + coffeQuantity +
+                ", coffeQuantity=" + coffeeQuantity +
                 '}';
     }
 }
