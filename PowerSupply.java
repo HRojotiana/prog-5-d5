33381703d923ece434b7040c3146d7f6979d9345
HRojotiana <hei.rojotiana@gmail.com>
2025-05-28 09:35:23 +0300

    feat: update PowerSystem

--- PowerSupply.java
+++ PowerSupply.java
@@ -1,3 +1,26 @@
 public class PowerSupply {
-    
+    protected boolean powered;
+
+    public PowerSupply(){
+        this.powered = false;
+    }
+
+    public boolean turn_on(){
+        this.powered = true;
+        return this.powered;
+    }
+
+    public boolean is_powered(){
+        if(!turn_on()) {
+            throw new RuntimeException("Power outage");
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "PowerSupply{" +
+                "powered=" + powered +
+                '}';
+    }
 }
